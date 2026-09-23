603c5eb7f464962469046511b5bc75e232eeb21d
santiagorey16 <srey175@unab.edu.co>
2026-09-23 15:40:05 -0500

    contador funcional

--- app/src/main/java/com/santiagorey/statesjetpackcompose/StateScreen.kt
+++ app/src/main/java/com/santiagorey/statesjetpackcompose/StateScreen.kt
@@ -1,5 +1,6 @@
 package com.santiagorey.statesjetpackcompose
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,14 +25,20 @@ import androidx.compose.ui.unit.sp
 )
 @Composable
 fun StateScreen() {
+
+    var contador by remember { mutableStateOf(0) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Text("Contador: 0", fontSize = 32.sp)
+        Text("Contador: $contador", fontSize = 32.sp)
         Spacer(modifier = Modifier.height(25.dp))
-        Button(onClick = {}) {
+        Button(onClick = {
+            Log.i("mis-errores", "Estoy dando click")
+            contador = contador + 1
+        }) {
             Text("Sumar 1")
         }
     }
