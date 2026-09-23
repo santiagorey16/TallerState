1946d3272478cd6c77ed239c84828f0016a025cc
santiagorey16 <srey175@unab.edu.co>
2026-09-23 15:09:13 -0500

    Arreglo de identación y maquetación

--- app/src/main/java/com/santiagorey/statesjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/santiagorey/statesjetpackcompose/MainActivity.kt
@@ -19,8 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-                }
-            }
         }
     }
 }
+
+
--- app/src/main/java/com/santiagorey/statesjetpackcompose/StateScreen.kt
+++ app/src/main/java/com/santiagorey/statesjetpackcompose/StateScreen.kt
@@ -1,2 +1,33 @@
 package com.santiagorey.statesjetpackcompose
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun StateScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Contador: 0", fontSize = 32.sp)
+        Spacer(modifier = Modifier.height(25.dp))
+        Button(onClick = {}) {
+            Text("Sumar 1")
+        }
+    }
+}
