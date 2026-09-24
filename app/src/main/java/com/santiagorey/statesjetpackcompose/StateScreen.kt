4065cca70cd69dbaa83e9db801ad9bff1e10cfaa
santiagorey16 <srey175@unab.edu.co>
2026-09-24 17:23:28 -0500

    dado funcional

--- app/src/main/java/com/santiagorey/statesjetpackcompose/StateScreen.kt
+++ app/src/main/java/com/santiagorey/statesjetpackcompose/StateScreen.kt
@@ -1,11 +1,13 @@
 package com.santiagorey.statesjetpackcompose
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,20 +30,36 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun StateScreen() {
 
-    var contador by remember { mutableStateOf(0) }
+    var contador by remember { mutableStateOf((1..6).random()) }
+    var lista = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6,
+    )
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Image(
+            painter = painterResource(id = lista[contador - 1]),
+            contentDescription = "es un dado",
+            modifier = Modifier
+                .height(200.dp)
+                .clip(CircleShape)
+        )
         Text("Contador: $contador", fontSize = 32.sp)
         Spacer(modifier = Modifier.height(25.dp))
         Button(onClick = {
             Log.i("mis-errores", "Estoy dando click")
             contador = contador + 1
+            contador = (1..6).random()
         }) {
-            Text("Sumar 1")
+            Text("Lanzar Dado!!")
         }
     }
 }
